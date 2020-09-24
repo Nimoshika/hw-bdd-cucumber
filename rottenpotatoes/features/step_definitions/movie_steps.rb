f1e012aba835c17100a88b9a63037e553741c5a1
EC2 Default User <ec2-user@ip-172-31-42-2.us-east-2.compute.internal>
2020-09-24 09:58:58 +0000

    first commit

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -23,7 +23,3 @@ Then /I should see all the movies/ do
     step %{I should see "#{movie.title}"}
   end
 end
-
-Then(/^the director of "(.*?)" should be "(.*?)"$/) do |title, director|
-  Movie.find_by_title(title).director.should eq director
-end
